706f9ed1bc5857d88cbb5fec8436e86945edcb2d
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2023-12-02 20:30:26 -0300

    feat: Criando as regras do jogo 🚀

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import quiz_questions from "../../../assets/data/quiz_questions.json"
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quiz',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.css'
 })
@@ -21,7 +23,35 @@ export class QuizComponent implements OnInit {
 
   finished:boolean = false;
 
+  constructor() {}
+  
   ngOnInit(): void {
+    if(quiz_questions){
+      this.finished = false
+      this.title = quiz_questions.title
 
+      this.questions = quiz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
+    }
+  }
+
+  playerChoice(value:string) {
+    this.answers.push(value);
+  }
+
+  async nextStep() {
+    this.questionIndex += 1;
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex]
+    }
+    else {
+      this.finished = true;
+    }
   }
 }
