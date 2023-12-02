c30b5ee30a802184b0cfc5fa07c579df78adb6b1
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2023-12-02 20:08:09 -0300

    feat: Criando propriedades lógicas 🚀

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quiz',
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.css'
 })
-export class QuizComponent {
 
+export class QuizComponent implements OnInit {
+  title: string = "";
+  questions: any;
+  questionSelected: any;
+
+  answers: string[] = [];
+  answerSelected:string = "";
+
+  questionIndex:number = 0;
+  questionMaxIndex:number = 0;
+
+  finished:boolean = false;
+
+  ngOnInit(): void {
+
+  }
 }
